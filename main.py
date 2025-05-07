402da2d320b5003c6657c174d4192c775e926366
bianca-rodrigues <biancapereirarodrigues02@gmail.com>
2025-05-07 15:40:22 -0300

    criacao do menu de navegacao

--- main.py
+++ main.py
@@ -1,56 +1,52 @@
-import os
-from dotenv import load_dotenv
-from supabase import create_client
-from fastapi import FastAPI
-import pandas as pd
-import matplotlib.pyplot as plt
+import streamlit as st
+from interface import pagina_login, cadastro, pagina_listagem, pagina_resgates, pagina_tratamento, pagina_doacoes, pagina_vacinas, pagina_adocao, pagina_func_volun
 
-app = FastAPI()
+menu = st.sidebar.selectbox("Navegar", ["Login", "Cadastro", "Listagem", "Resgates", "Tratamentos", "Doações", "Vacinas", "Adoção", "Cadastro Funcionários", "Sair"])
 
-# Carrega as variáveis do arquivo .env
-load_dotenv()
-SUPABASE_URL = os.getenv("SUPABASE_URL")
-SUPABASE_KEY = os.getenv("SUPABASE_KEY")
-
-# Cria o cliente Supabase
-supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
-
-@app.get("/")
-def root():
-    return {"mensagem": "API do Canil está rodando!"}
-
-# Função para listar todos os animais
-def listar_animais():
-    resposta = supabase.table("animais").select("*").execute()
-    print("Animais cadastrados:")
-    print(resposta.data)
-
-# Função para cadastrar um novo animal
-def cadastrar_animal(nome, especie, raca, porte, sexo, idade, cor, dataentrada, status):
-    novo = {
-        "nome": nome,
-        "especie": especie,
-        "raca": raca,
-        "porte": porte,
-        "sexo": sexo,
-        "idade": idade,
-        "cor": cor,
-        "dataentrada": dataentrada,
-        "status": False
-    }
-    resposta = supabase.table("animais").insert(novo).execute()
-    print("Animal cadastrado:")
-    print(resposta.data)
-
-# Função para atualizar um animal para 'adotado'
-def marcar_como_adotado(id_animal):
-    resposta = supabase.table("animais").update({"adotado": True}).eq("id", id_animal).execute()
-    print("Animal atualizado:")
-    print(resposta.data)
-
-# Função para deletar um animal por ID
-def deletar_animal(id_animal):
-    resposta = supabase.table("animais").delete().eq("id", id_animal).execute()
-    print("Animal removido:")
-    print(resposta.data)
-
+if menu == "Login":
+    pagina_login()
+elif menu == "Cadastro":
+    if "usuario" in st.session_state:
+        cadastro()
+    else:
+        st.warning("Faça login primeiro.")
+elif menu == "Listagem":
+    if "usuario" in st.session_state:
+        pagina_listagem()
+    else:
+        st.warning("Faça login primeiro")
+elif menu == "Resgates":
+    if "usuario" in st.session_state:
+        pagina_resgates()
+    else:
+        st.warning("Faça login primeiro")
+elif menu == "Tratamentos":
+    if "usuario" in st.session_state:
+        pagina_tratamento()
+    else:
+        st.warning("Faça login primeiro")
+elif menu == "Doações":
+    if "usuario" in st.session_state:
+        pagina_doacoes()
+    else:
+        st.warning("Faça login primeiro")
+elif menu == "Vacinas":
+    if "usuario" in st.session_state:
+        pagina_vacinas()
+    else:
+        st.warning("Faça login primeiro")
+elif menu == "Adoção":
+    if "usuario" in st.session_state:
+        pagina_adocao()
+    else:
+        st.warning("Faça login primeiro")
+elif menu == "Cadastro Funcionários":
+    if "usuario" in st.session_state:
+        pagina_func_volun()
+    else:
+        st.warning("Faça login primeiro")
+elif menu == "Sair":
+    if "usuario" in st. session_state:
+        st.logout()
+    else:
+        st.warning("Você não está conectado")
